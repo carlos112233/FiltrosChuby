2d9dd8583bed4f9c9ec6139be5b8c2372f106ee1
upam <carlos.araiza#>
2017-02-08 09:56:44 -0600

    Primer Proyecto

--- app/src/main/java/com/wanosoft/filtroschuby/MainActivity.java
+++ app/src/main/java/com/wanosoft/filtroschuby/MainActivity.java
@@ -1,7 +1,10 @@
 package com.wanosoft.filtroschuby;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageView imageView = (ImageView) findViewById(R.id.imagen);
+
+        filtro filtr = new filtro();
+
+        BitmapDrawable bitmapDrawable=(BitmapDrawable) imageView.getDrawable();
+        //Bitmap bmp=filtros.greyScale(bitmapDrawable.getBitmap());
+        Bitmap bmp=filtr.invert(bitmapDrawable.getBitmap());
+        imageView.setImageBitmap(bmp);
     }
 }
